4e1a03da40066a0d9faac0d85b655e72c94e38a7
Shyam Sundar Bharathi <77499506+Shyam-Sundar-Bharathi@users.noreply.github.com>
2021-05-16 21:23:57 +0530

    Deleted samples

--- DreamCalc/lib/calcs/currencyCalc.dart
+++
@@ -1,7 +0,0 @@
-import 'package:dream_calc/services/formatNumber.dart';
-
-String currencyCalc(String userInput, double exchangeRate){
-  if(userInput == '')
-    return '';
-  return formatNumber((double.parse(userInput)*exchangeRate),isCurrency: true);
-}
